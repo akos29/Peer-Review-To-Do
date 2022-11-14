a7e083609884cec1a671153e0eb49e88c8a3f2a1
unknown <kibrish49@gmail.com>
2022-11-14 15:21:22 +0200

    Add ReadMe file

--- src/modules/refresh.js
+++
@@ -1,9 +0,0 @@
-import * as el from './elementSelector.js';
-import generateTask from './generateTaskList.js';
-import Tasks from './tasks.js';
-
-export default function refresh() {
-  Tasks.sortTask();
-  el.container.innerHTML = '';
-  return generateTask();
-}
